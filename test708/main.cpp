e922853f3f79afb382a8a70a1010b2b958fa55d0
李晨曦 <lichenxi@lichenxidemacbook-pro.local>
2018-07-08 23:34:00 +0800

    Initial Commit

---
+++ test708/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test708
+//
+//  Created by 李晨曦 on 2018/7/8.
+//  Copyright © 2018年 李晨曦. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
